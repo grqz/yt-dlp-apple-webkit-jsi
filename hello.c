67e8af2f67398c446a4fc4ec318edabeeef398d2
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-02 12:49:28 +1200

    Dont load foundations

--- hello.c
+++ hello.c
@@ -11,72 +11,74 @@
 // NSAddImage
 // objc:
 // NSBundle
-// message.h
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 typedef void *(*FnProto_objc_getClass)(const char *name);
-typedef void (*FnProto_objc_msgSend)(void *self, void *op, ...);
-typedef void *(*FnProto_objc_msgSend_vp_)(void *self, void *op);
-typedef void *(*FnProto_objc_msgSend_vp_vp)(void *self, void *op, void *);
+
+typedef void (*FnProto_objc_msgSend)();
+typedef void (*FnProtov_objc_msgSend)(void *self, void *op);
+typedef void *(*FnProtovp_objc_msgSend)(void *self, void *op);
+typedef void *(*FnProtovp_vp_objc_msgSend)(void *self, void *op, void *);
 
 typedef void *(*FnProto_sel_registerName)(const char * str);
 
 typedef void (*FnProto_NSLog)(void *format, ...);
 
-#ifdef __cplusplus
-}
-#endif
-
 int main(void) {
+    char nul = 0;
     int ret = 1;
-    void *objc = dlopen("/usr/lib/libobjc.A.dylib", RTLD_LAZY);
+    void *objc = dlopen("/usr/lib/libobjc.A.dylib", RTLD_NOW);
     if (!objc) {
-        fprintf(stderr, "Failed to load libobjc, are you on APPLE?\n");
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to load libobjc: %s; Are you on APPLE?\n", errm ? errm : &nul);
         goto end0;
     }
     FnProto_objc_getClass objc_getClass = dlsym(objc, "objc_getClass");
     if (!objc_getClass) {
-        fprintf(stderr, "Failed to get objc_getClass\n");
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to get objc_getClass: %s\n", errm ? errm : &nul);
         goto end1;
     }
     FnProto_objc_msgSend objc_msgSend = dlsym(objc, "objc_msgSend");
     if (!objc_msgSend) {
-        fprintf(stderr, "Failed to get objc_msgSend\n");
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to get objc_msgSend: %s\n", errm ? errm : &nul);
         goto end1;
     }
     FnProto_sel_registerName sel_registerName = dlsym(objc, "sel_registerName");
     if (!sel_registerName) {
-        fprintf(stderr, "Failed to get sel_registerName\n");
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to get sel_registerName: %s\n", errm ? errm : &nul);
         goto end1;
     }
 
-    void *foundation = dlopen("/System/Library/Frameworks/Foundation.framework/Foundation", RTLD_LAZY);
-    if (!foundation) {
-        fprintf(stderr, "Failed to load Foundation, is it in the right place?\n");
-        goto end1;
-    }
+    // void *foundation = dlopen("/System/Library/Frameworks/Foundation.framework/Foundation", RTLD_LAZY);
+    // if (!foundation) {
+    //     const char *errm = dlerror();
+    //     fprintf(stderr, "Failed to load Foundation: %s; Is it in the right place?\n", errm ? errm : &nul);
+    //     goto end1;
+    // }
     fprintf(stderr, "All libraries loaded\n");
 
-    FnProto_NSLog NSLog = dlsym(foundation, "NSLog");
+    // FnProto_NSLog NSLog = dlsym(foundation, "NSLog");
+    FnProto_NSLog NSLog = dlsym(objc, "NSLog");
     if (!NSLog) {
-        fprintf(stderr, "Failed to load from Foundation: NSLog");
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to get NSLog from Foundation: %s\n", errm ? errm : &nul);
         goto end2;
     }
+
     void *ClsNSString = objc_getClass("NSString");
     if (!ClsNSString) {
-        fprintf(stderr, "Failed to get NSString\n");
+        fprintf(stderr, "Failed to getClass NSString\n");
         goto end2;
     }
-    void *msgAlloc = sel_registerName("alloc");
-    void *msgInit = sel_registerName("init");
-    void *msgDealloc = sel_registerName("dealloc");
+    void *selAlloc = sel_registerName("alloc");
+    void *selInit = sel_registerName("init");
+    void *selRelease = sel_registerName("release");
     fprintf(stderr, "Initialised selectors\n");
-    void *pStr = ((FnProto_objc_msgSend_vp_)objc_msgSend)(ClsNSString, msgAlloc);
+    void *pStr = ((FnProtovp_objc_msgSend)objc_msgSend)(ClsNSString, selAlloc);
     fprintf(stderr, "Allocated NSString\n");
-    pStr = ((FnProto_objc_msgSend_vp_vp)objc_msgSend)(pStr, sel_registerName("initWithUTF8String:"), "Hello, World");
+    pStr = ((FnProtovp_vp_objc_msgSend)objc_msgSend)(pStr, sel_registerName("initWithUTF8String:"), "Hello, World");
     fprintf(stderr, "Initialised NSString\n");
 
     NSLog(pStr);
@@ -84,11 +86,17 @@ int main(void) {
 
     ret = 0;
 end3:
-    objc_msgSend(pStr, msgDealloc);
+    ((FnProtov_objc_msgSend)objc_msgSend)(pStr, selRelease);
 end2:
-    dlclose(foundation);
+    // if (dlclose(foundation)) {
+    //     const char *errm = dlerror();
+    //     fprintf(stderr, "Failed to dlclose Foundation: %s\n", errm ? errm : &nul);
+    // }
 end1:
-    dlclose(objc);
+    if (dlclose(objc)) {
+        const char *errm = dlerror();
+        fprintf(stderr, "Failed to dlclose Foundation: %s\n", errm ? errm : &nul);
+    }
 end0:
     return ret;
 }
