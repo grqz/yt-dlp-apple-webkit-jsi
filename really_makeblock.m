c606585bc791998a9ba1a0823678f39ff584e362
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-03 12:24:49 +1200

    stage

---
+++ really_makeblock.m
@@ -0,0 +1,9 @@
+#include <stddef.h>
+#import "fn_to_block.h"
+
+void *really_makeblock_cbv_2vp(void (*fnptr)(void *, void *, void *), void *userData) {
+    void (^__block block)(void *, void *) = [^(void *a, void *b) {
+        fnptr((void *)block, a, b);
+    } copy];
+    return block;
+}
