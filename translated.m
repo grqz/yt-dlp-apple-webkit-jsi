343a983b8a7565a2a218dd997dfd02615fb5fc90
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-03 14:02:11 +1200

    did i not stage

---
+++ translated.m
@@ -0,0 +1,67 @@
+#import <Foundation/Foundation.h>
+#import <WebKit/WebKit.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+#include "config.h"
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+BOOL stop = NO;
+
+static inline
+void onCallAsyncJSComplete(void *idResult, void *nserrError) {
+    fprintf(stderr, "JS Complete! idResult: %p; nserrError: %p\n", idResult, nserrError);
+    if (!idResult) return;
+    pthread_mutex_lock(&mtx);
+    stop = YES;
+    pthread_cond_signal(&cv);
+    pthread_mutex_unlock(&mtx);
+}
+
+int main(void) {
+    WKWebViewConfiguration *pCfg = [[WKWebViewConfiguration alloc] init];
+    void *pPref = [pCfg preferences];
+    [pPref setJavaScriptCanOpenWindowsAutomatically:YES];
+    NSString *psSetKey = [[NSString alloc] initWithUTF8String:"allowFileAccessFromFileURLs"];
+    [pPref setValue:kCFBooleanTrue forKey:psSetKey];
+    [psSetKey release]; psSetKey = nil;
+    pPref = nil;
+    psSetKey = [[NSString alloc] initWithUTF8String:"allowUniversalAccessFromFileURLs"];
+    [pCfg setValue:kCFBooleanTrue forKey:psSetKey];
+    [psSetKey release]; psSetKey = nil;
+    WKWebView *pWebview = [[WKWebView alloc] initWithFrame:CGRectZero configuration:pCfg];
+    [pCfg release]; pCfg = nil;
+
+    NSString *psHTMLString = [[NSString alloc] initWithUTF8String:szHTMLString];
+    NSString *psBaseURL = [[NSString alloc] initWithUTF8String:szBaseURL];
+    NSURL *pnurlBaseURL = [[NSURL alloc] initWithString:psBaseURL];
+    [pWebview loadHTMLString:psHTMLString baseURL:pnurlBaseURL];
+    [pnurlBaseURL release]; pnurlBaseURL = nil;
+    [psBaseURL release]; psBaseURL = nil;
+    [psHTMLString release]; psHTMLString = nil;
+
+    NSString *psScript = [[NSString alloc] initWithUTF8String:szScript];
+    NSDictionary *pdJsArguments = [[NSDictionary alloc] init];
+    void *rpPageWorld = [WKContentWorld pageWorld];
+    [pWebview callAsyncJavaScript:psScript
+        arguments:pdJsArguments
+        inFrame:nil
+        inContentWorld:rpPageWorld
+        completionHandler:^ (void *idResult, void *nserrError) {
+            onCallAsyncJSComplete(idResult, nserrError);
+        }];
+    [pdJsArguments release]; pdJsArguments = nil;
+    [psScript release]; psScript = nil;
+    // wait until completionHandler is called
+    pthread_mutex_lock(&mtx);
+    while (!stop)
+        pthread_cond_wait(&cv, &mtx);
+    pthread_mutex_unlock(&mtx);
+    [pWebview release]; pWebview = nil;
+    fprintf(stderr, "Finished\n");
+    pthread_mutex_destroy(&mtx);
+    pthread_cond_destroy(&cv);
+    return 0;
+}
