5d0a8dbce41797ca9e7d80c59bbfb83b29b34766
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-09 16:45:13 +1200

    print signature

--- testcall.m
+++ testcall.m
@@ -1,12 +1,19 @@
 #import <CoreFoundation/CoreFoundation.h>
 #include <stdio.h>
+#include "fn_to_block.h"
 
 int main(void) {
     void * __block ml = CFRunLoopGetMain();
-    CFRunLoopPerformBlock(ml, kCFRunLoopDefaultMode, ^{
+    void (^blk)(void) = ^{
         fputs("CFRunLoopPerformBlock inner\n", stderr);
         CFRunLoopStop(ml);
-    });
+    };
+    void *pblock;
+    *((void **)&pblock) = blk;
+    char *signature = (char *)signatureof(pblock);
+    if (!signature) signature = "";
+    fprintf(stderr, "signature(%s)\n", signature);
+    CFRunLoopPerformBlock(ml, kCFRunLoopDefaultMode, blk);
     CFRunLoopRun();
     fputs("main finish\n", stderr);
     return 0;
