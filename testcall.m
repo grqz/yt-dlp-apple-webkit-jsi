3ebb8ccbb6df9bc32462fdc4c9ca392b3e7da4f1
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-09 17:10:23 +1200

    ?

--- testcall.m
+++ testcall.m
@@ -21,9 +21,7 @@ int main(void) {
         else
             (((struct Prototype_BlockDescSign *)baseBlock->desc)->signature) = "v@?";
     }
-    void (^blk_newsig)(void);
-    *(void (^)(void) *)&blk_newsig = baseBlock;
-    CFRunLoopPerformBlock(ml, kCFRunLoopDefaultMode, baseBlock);
+    ((void (*)(void *, void *, void *))CFRunLoopPerformBlock)(ml, kCFRunLoopDefaultMode, baseBlock);
     [baseBlock release];
     CFRunLoopRun();
     fputs("main finish\n", stderr);
