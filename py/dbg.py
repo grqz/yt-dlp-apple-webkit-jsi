034b003a596b7ec3a75c6ec312f5a292e9329910
_Grqz <173015200+grqz@users.noreply.github.com>
2025-09-11 08:52:20 +1200

    assertions

--- py/dbg.py
+++ py/dbg.py
@@ -1,15 +1,21 @@
-from ctypes import c_bool
-from .pyneapple_objc import PyNeApple, debug_log
+from ctypes import c_bool, c_void_p
+from .pyneapple_objc import PyNeApple, cfn_at, debug_log
 from .run import CGRect
 
 with PyNeApple() as pa:
     pa.load_framework_from_path('Foundation')
+    cf = pa.load_framework_from_path('CoreFoundation')
     pa.load_framework_from_path('WebKit')
 
     NSThread = pa.safe_objc_getClass(b'NSThread')
     WKWebView = pa.safe_objc_getClass(b'WKWebView')
 
-    assert pa.send_message(NSThread, b'isMainThread', restype=c_bool), 'not on main?'
+    i_lcurrent = cfn_at(cf(b'CFRunLoopGetCurrent').value, c_void_p)()
+    i_lmain = cfn_at(cf(b'CFRunLoopGetMain').value, c_void_p)()
+
+    assert i_lcurrent == i_lmain, 'current loop not main'
+
+    assert pa.send_message(NSThread, b'isMainThread', restype=c_bool), 'not on main thread'
 
     therect = CGRect()
     p = pa.safe_new_object(WKWebView, b'initWithFrame:', therect, argtypes=(CGRect, ))
@@ -17,3 +23,7 @@ with PyNeApple() as pa:
     debug_log(f'{p=}')
 
 debug_log('survived')
+
+# RELEASE_ASSERT(isMainRunLoop());
+# this assertion failed
+# <https://github.com/WebKit/WebKit/blob/1a1fa431f8a684f9b88c05fef1ea9b638fb5f04b/Source/WebKit/UIProcess/Cocoa/WebProcessPoolCocoa.mm#L951>
