58af0830627b7efd034e545c7246ddd60a10e528
grchz <173015200+grqz@users.noreply.github.com>
2025-09-22 17:34:24 +1200

    typeof test

--- py/config.py
+++ py/config.py
@@ -223,7 +223,7 @@ const minter = await (async (integrityTokenResponse, webPoSignalOutput_) => {
 // console.log(`visitorData(generated with Innertube): ${visitorData}`);
 // console.log(`GVS: ${await minter(visitorData)}`);
 const pot = await minter(globalThis?.process?.argv[2] || 'dQw4w9WgXcQ');
-return {result: 'success', debugInfo: [document.URL], data: pot};
+return {result: 'success', debugInfo: [document.URL], data: pot, tofn: typeof window.webkit?.messageHandlers?.pywk?.postMessage};
 } catch(e) {return {result: 'error', debugInfo: [document.URL], error: e};}
 })();
 '''.encode()
