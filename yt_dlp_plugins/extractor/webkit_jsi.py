82b08f78662f1d8b5d42ca66475fcd372100320e
grchz <173015200+grqz@users.noreply.github.com>
2025-10-06 22:34:03 +1300

    test5k

--- yt_dlp_plugins/extractor/webkit_jsi.py
+++ yt_dlp_plugins/extractor/webkit_jsi.py
@@ -58,7 +58,7 @@ class AppleWebKitJCP(JsRuntimeChalBaseJCP):
                 result += str_to_log
 
         script = '(()=>{const a = 3; let b = 4; function c(){return Array.from(arguments);} const d = JSON.stringify(c(a,b)); console.log([null, d])})(); if(0){' + stdin + '}'
-        last500 = script[-150:]
+        last500 = script[-5000:]
         self.logger.info(f'started solving challenge, {len(script)=}, {last500.encode()}')
         # TODO: cached facory/webview
         with WKJSE_Factory(Logger(debug=True)) as send, WKJSE_Webview(send) as webview:
