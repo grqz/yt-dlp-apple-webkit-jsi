a8c9b4a23cefd754e031804202fe2f9d64a77948
grchz <173015200+grqz@users.noreply.github.com>
2025-10-21 21:28:16 +1300

    brief test

--- yt_dlp_plugins/webkit_jsi/examples/config.py
+++ yt_dlp_plugins/webkit_jsi/examples/config.py
@@ -2,6 +2,7 @@ HTML = r'''<!DOCTYPE html><html lang="en"><head><title></title></head><body></bo
 HOST = r'''https://www.youtube.com/robots.txt'''
 SCRIPT = r'''
 try {
+console.log(typeof globalThis.XMLHttpRequest, typeof globalThis.window, typeof URL, typeof globalThis.document, typeof globalThis.navigator, typeof globalThis.self);
 const val = await Promise.all([3, null, true, new Date, false].map(communicate));
 console.log('started generating pot, communicate result: ', val);
 // pot for browser, navigate to https://www.youtube.com/robots.txt first
