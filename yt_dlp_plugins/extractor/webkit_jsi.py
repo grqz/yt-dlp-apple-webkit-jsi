dc9f5e8cc990d46213261d0f999c972fc53def9f
grchz <173015200+grqz@users.noreply.github.com>
2025-10-22 13:27:26 +1300

    actually 0.0.7, did not save

--- yt_dlp_plugins/extractor/webkit_jsi.py
+++ yt_dlp_plugins/extractor/webkit_jsi.py
@@ -9,7 +9,7 @@ from ..webkit_jsi.lib.easy import WKJSE_Factory, WKJSE_Webview
 from ..webkit_jsi.lib.api import DarwinMinVer
 
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 
 FACTORY_CACHE_TYPE = WKJSE_Factory
