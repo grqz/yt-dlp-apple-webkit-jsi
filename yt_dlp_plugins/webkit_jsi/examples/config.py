a071cac08e01e453d4b11fb5dc202aa7ec40bab3
grchz <grqz.dev@proton.me>
2026-04-25 14:53:23 +1200

    yt-dlp-winz

--- yt_dlp_plugins/webkit_jsi/examples/config.py
+++ yt_dlp_plugins/webkit_jsi/examples/config.py
@@ -1,8 +1,8 @@
 HTML = r'''<!DOCTYPE html><html lang="en"><head><title></title></head><body></body></html>'''
-HOST = r'''https://www.youtube.com/robots.txt'''
+HOST = r'''https://www.youtube.com/watch?v=yt-dlp-wins'''
 SCRIPT = r'''
 try {
-window.location.href = 'https://www.youtube.com/watch?v=yt-dlp-wins';
+window.location.href = 'https://www.youtube.com/watch?v=yt-dlp-winz';
 console.log(window.location.href);
 console.log(typeof globalThis.XMLHttpRequest, typeof globalThis.window, typeof URL, typeof globalThis.document, typeof globalThis.navigator, typeof globalThis.self);
 const val = await Promise.all([3, null, true, new Date, false].map(communicate));
