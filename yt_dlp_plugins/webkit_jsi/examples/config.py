d2d42d8d9de7a25e8828d8c79e5fa6c9d7ee971a
grchz <grqz.dev@proton.me>
2026-04-25 14:42:52 +1200

    fix wrapper script, test set window location

--- yt_dlp_plugins/webkit_jsi/examples/config.py
+++ yt_dlp_plugins/webkit_jsi/examples/config.py
@@ -2,6 +2,8 @@ HTML = r'''<!DOCTYPE html><html lang="en"><head><title></title></head><body></bo
 HOST = r'''https://www.youtube.com/robots.txt'''
 SCRIPT = r'''
 try {
+window.location.href = 'https://www.youtube.com/robots.txt';
+console.log(window.location.href);
 console.log(typeof globalThis.XMLHttpRequest, typeof globalThis.window, typeof URL, typeof globalThis.document, typeof globalThis.navigator, typeof globalThis.self);
 const val = await Promise.all([3, null, true, new Date, false].map(communicate));
 console.log('started generating pot, communicate result: ', val);
